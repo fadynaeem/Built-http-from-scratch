5ff1e59d52bf6f66c7f9e649bbd150d592d1ad76
Fady Naeem <99994404+fadynaeem@users.noreply.github.com>
2025-11-05 21:27:13 +0200

    (version1) http-server-project

---
+++ include/Sockets.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+using socklen_t = int;
+#else
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <arpa/inet.h>
+  #include <unistd.h>
+  #include <netdb.h>
+#endif
