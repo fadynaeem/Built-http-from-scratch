d9ceeb2ae81cfa5e4ed10f1ff60ea73b4b41997c
Fady Naeem <99994404+fadynaeem@users.noreply.github.com>
2025-11-05 21:34:06 +0200

    (version1) http-server-project

---
+++ src/core/app.cpp
@@ -0,0 +1,14 @@
+#include "../../include/core/App.h"
+App::App(int port, int num_threads) : server_(port, num_threads) {
+}
+void App::run() {
+    server_.start();
+}
+void App::stop() {
+}
+Router& App::router() {
+    return server_.getRouter();
+}
+void App::use(std::function<void(Request&, Response&, std::function<void()>)> middleware) {
+    server_.use(middleware);
+}
